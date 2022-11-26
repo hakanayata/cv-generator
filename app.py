80d112268366c2ed8fc6065e6d7c0840fd08936f
Hakan <s.hakanayata@gmail.com>
2022-11-26 23:31:17 +0100

    visual fix for personal info part

--- app.py
+++ app.py
@@ -38,13 +38,13 @@ def index():
         pdf.set_text_color(250, 250, 250)
         # page width 210mm but default margin 1cm from left + right
         pdf.cell(
-            w=190, h=10, txt=f"{first_name} {last_name}", align="C", fill=True, new_x="LMARGIN", new_y="NEXT")
+            w=190, h=12, txt=f"{first_name} {last_name}", align="C", fill=True, new_x="LMARGIN", new_y="NEXT")
         # ! Contact
-        pdf.set_font("Helvetica", size=12)
+        pdf.set_font("Helvetica", "", size=12)
         pdf.set_fill_color(20, 20, 100)
         pdf.set_text_color(250, 250, 250)
         pdf.cell(
-            w=190, txt=f"{address} | {phone_number} | {email}", align="C", fill=True)
+            w=190, h=6, txt=f"{address} | {phone_number} | {email}", align="C", fill=True)
         # title of pdf
         pdf.set_title(f"{last_name}-CV")
         response = make_response(pdf.output())
