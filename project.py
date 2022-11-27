7cd8c2e15a56df2213d9fedabdb1d1695835a857
Hakan <s.hakanayata@gmail.com>
2022-11-27 14:01:17 +0100

    last commit before test branch

--- project.py
+++ project.py
@@ -1,17 +1,20 @@
+from app import index
+
+
 def main():
-    ...
+    return index
 
 
-def function_1():
-    ...
+# def function_1():
+#     ...
 
 
-def function_2():
-    ...
+# def function_2():
+#     ...
 
 
-def function_n():
-    ...
+# def function_n():
+#     ...
 
 
 if __name__ == "__main__":
